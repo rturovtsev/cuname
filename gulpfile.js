6e76634477f5a5a5aa3dbd4aedfa647471b8e73d
sugrob69 <sugrob.boid@gmail.com>
2016-03-23 13:11:09 +0300

    webpack, react

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,21 @@
 'use strict';
 
-const gulp = require('gulp'),
+const path = require('path'),
+    gulp = require('gulp'),
     uglify = require('gulp-uglify'),
+    gulpif = require('gulp-if'),
     server = require('gulp-express'),
     webpackStream = require('webpack-stream'),
     webpack = webpackStream.webpack,
     named = require('vinyl-named'),
     isDevelopment = !process.env.NODE_ENV || process.env.NODE_ENV == 'development',
+    NODE_ENV = process.env.NODE_ENV || 'development',
     serveOptions = {
         cwd: undefined
     };
 
 serveOptions.env = process.env;
-serveOptions.env.NODE_ENV = 'development';
+serveOptions.env.NODE_ENV = NODE_ENV ;
 serveOptions.env.DEBUG = 'cuname:*';
 
 gulp.task('server', function () {
@@ -27,20 +30,34 @@ gulp.task('webpack', function(){
         },
         devtool: isDevelopment ? "source-map" : null,
         module: {
+            preLoaders: [
+                {
+                    test: /\.js$/,
+                    loaders: ['eslint'],
+                    include: [
+                        path.resolve(__dirname, "src")
+                    ]
+                }
+            ],
             loaders: [{
                 test: /\.js$/,
-                loader: 'babel?presets[]=es2015'
+                loader: 'babel?presets[]=es2015,plugins[]=transform-es2015-modules-commonjs',
+                plugins: ['transform-runtime']
             }]
         },
         plugins: [
-            new webpack.NoErrorsPlugin()
+            new webpack.NoErrorsPlugin(),
+            new webpack.DefinePlugin({
+                NODE_ENV: JSON.stringify(NODE_ENV),
+                LANG: JSON.stringify('ru')
+            })
         ]
     };
 
     return gulp.src('./js_modules/main.js')
         .pipe(named())
         .pipe(webpackStream(webpackOptions))
-        //.pipe(uglify())
+        .pipe(gulpif(!isDevelopment, uglify()))
         .pipe(gulp.dest('./public/js/'));
 });
 
